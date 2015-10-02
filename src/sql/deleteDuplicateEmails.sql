2537152983c7b156fb26f9db7f67e15870dc0401
shiyiwan <shiyiwan@gmail.com>
2015-10-02 11:18:23 +0800

    Add 'select duplicate EMail'

--- src/sql/deleteDuplicateEmails.sql
+++ src/sql/deleteDuplicateEmails.sql
@@ -1,1 +1,12 @@
-DELETE p1 FROM Person p1 Inner join Person p2 WHERE p1.Email = p2.Email AND p1.Id > p2.Id; 
+DELETE p1 
+  FROM Person p1 
+ INNER JOIN Person p2 
+ WHERE p1.Email = p2.Email 
+   AND p1.Id > p2.Id; 
+   
+# Write your MySQL query statement below
+SELECT distinct p1.Email 
+  FROM Person p1 
+ INNER JOIN Person p2 
+ WHERE p1.Email = p2.Email 
+   AND p1.id <> p2.id;
